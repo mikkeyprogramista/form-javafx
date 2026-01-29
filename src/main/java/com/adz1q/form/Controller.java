fe2dee647db1f3088bfd2106c29b377fee8ce891
mikkeyprogramista <mikkeyprogramista@gmail.com>
2026-01-29 12:12:01 +0100

    Add complete project

--- src/main/java/com/adz1q/form/Controller.java
+++ src/main/java/com/adz1q/form/Controller.java
@@ -1,14 +1,41 @@
 package com.adz1q.form;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TextField txtFirstName;
+
+    @FXML
+    private TextField txtLastName;
+
+    @FXML
+    private CheckBox chx1;
+
+    @FXML
+    private CheckBox chx2;
+
+    @FXML
+    private CheckBox chx3;
+
+    @FXML
+    private CheckBox chx4;
+
+    @FXML
+    private Button btnGet;
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnClose;
+
+    @FXML
+    private TextArea txtDescription;
+
 }
